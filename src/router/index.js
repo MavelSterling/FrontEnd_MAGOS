a7596fd86062f6783f87c52ef1f6f4afe20f84d5
MavelSterling <mavelynsl@gmail.com>
2022-11-13 17:45:55 -0500

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Dashboard from "../views/Dashboard.vue";
 import Tables from "../views/Tables.vue";
 import Billing from "../views/Billing.vue";
-import VirtualReality from "../views/VirtualReality.vue";
+//import VirtualReality from "../views/VirtualReality.vue";
 import RTL from "../views/Rtl.vue";
 import Profile from "../views/Profile.vue";
 import Signup from "../views/Signup.vue";
@@ -29,11 +29,11 @@ const routes = [
     name: "Billing",
     component: Billing,
   },
-  {
+  /*{
     path: "/virtual-reality",
     name: "Virtual Reality",
     component: VirtualReality,
-  },
+  },*/
   {
     path: "/rtl-page",
     name: "RTL",
