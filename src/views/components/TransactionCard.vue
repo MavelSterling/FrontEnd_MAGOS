01f69e0070ec39a3ea0efafc87846f327e4021e4
MavelSterling <mavelynsl@gmail.com>
2022-11-14 02:14:51 -0500

    Se agrega los nombre en las transacciones de ahorros

--- src/views/components/TransactionCard.vue
+++ src/views/components/TransactionCard.vue
@@ -3,16 +3,16 @@
     <div class="card-header pb-0 px-3">
       <div class="row">
         <div class="col-md-6">
-          <h6 class="mb-0">Your Transaction's</h6>
+          <h6 class="mb-0">Transacciones de la cuenta</h6>
         </div>
         <div class="col-md-6 d-flex justify-content-end align-items-center">
           <i class="far fa-calendar-alt me-2" aria-hidden="true"></i>
-          <small>23 - 30 March 2020</small>
+          <small>20 - 30 Nov 2022</small>
         </div>
       </div>
     </div>
     <div class="card-body pt-4 p-3">
-      <h6 class="text-uppercase text-body text-xs font-weight-bolder mb-3">Newest</h6>
+      <h6 class="text-uppercase text-body text-xs font-weight-bolder mb-3">Nuevas</h6>
       <ul class="list-group">
         <li
           class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg"
@@ -28,7 +28,7 @@
             </argon-button>
             <div class="d-flex flex-column">
               <h6 class="mb-1 text-dark text-sm">Netflix</h6>
-              <span class="text-xs">27 March 2020, at 12:30 PM</span>
+              <span class="text-xs">27 Octubre 2022, Hora: 12:30 PM</span>
             </div>
           </div>
           <div
@@ -49,7 +49,7 @@
             </argon-button>
             <div class="d-flex flex-column">
               <h6 class="mb-1 text-dark text-sm">Apple</h6>
-              <span class="text-xs">27 March 2020, at 04:30 AM</span>
+              <span class="text-xs">27 Octubre 2022, Hora: 04:30 AM</span>
             </div>
           </div>
           <div
@@ -57,7 +57,7 @@
           >+ $ 2,000</div>
         </li>
       </ul>
-      <h6 class="text-uppercase text-body text-xs font-weight-bolder my-3">Yesterday</h6>
+      <h6 class="text-uppercase text-body text-xs font-weight-bolder my-3">Ayer</h6>
       <ul class="list-group">
         <li
           class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg"
@@ -72,55 +72,55 @@
               <i class="fas fa-arrow-up" aria-hidden="true"></i>
             </argon-button>
             <div class="d-flex flex-column">
-              <h6 class="mb-1 text-dark text-sm">Stripe</h6>
-              <span class="text-xs">26 March 2020, at 13:45 PM</span>
+              <h6 class="mb-1 text-dark text-sm">Platzi</h6>
+              <span class="text-xs">26 Octubre 2022, Hora: 13:45 PM</span>
             </div>
           </div>
           <div
             class="d-flex align-items-center text-success text-gradient text-sm font-weight-bold"
-          >+ $ 750</div>
+          >+ $ 7,050</div>
         </li>
         <li
           class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg"
         >
           <div class="d-flex align-items-center">
             <argon-button
-              color="success"
+              color="danger"
               variant="outline"
               size="sm"
               class="btn-icon-only btn-rounded mb-0 me-3 d-flex align-items-center justify-content-center"
             >
-              <i class="fas fa-arrow-up" aria-hidden="true"></i>
+              <i class="fas fa-arrow-down" aria-hidden="true"></i>
             </argon-button>
             <div class="d-flex flex-column">
-              <h6 class="mb-1 text-dark text-sm">HubSpot</h6>
-              <span class="text-xs">26 March 2020, at 12:30 PM</span>
+              <h6 class="mb-1 text-dark text-sm">Fortnite</h6>
+              <span class="text-xs">26 Octubre 2022, Hora: 12:30 PM</span>
             </div>
           </div>
           <div
-            class="d-flex align-items-center text-success text-gradient text-sm font-weight-bold"
-          >+ $ 1,000</div>
+            class="d-flex align-items-center text-danger text-gradient text-sm font-weight-bold"
+          >- $ 2,000</div>
         </li>
         <li
           class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg"
         >
           <div class="d-flex align-items-center">
             <argon-button
-              color="success"
+              color="danger"
               variant="outline"
               size="sm"
               class="btn-icon-only btn-rounded mb-0 me-3 d-flex align-items-center justify-content-center"
             >
-              <i class="fas fa-arrow-up" aria-hidden="true"></i>
+              <i class="fas fa-arrow-down" aria-hidden="true"></i>
             </argon-button>
             <div class="d-flex flex-column">
-              <h6 class="mb-1 text-dark text-sm">Creative Tim</h6>
-              <span class="text-xs">26 March 2020, at 08:30 AM</span>
+              <h6 class="mb-1 text-dark text-sm">Call of Duty</h6>
+              <span class="text-xs">26 Octubre 2022, Hora: 08:30 AM</span>
             </div>
           </div>
           <div
-            class="d-flex align-items-center text-success text-gradient text-sm font-weight-bold"
-          >+ $ 2,500</div>
+            class="d-flex align-items-center text-danger text-gradient text-sm font-weight-bold"
+          >- $ 1,500</div>
         </li>
         <li
           class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg"
@@ -135,11 +135,11 @@
               <i class="fas fa-exclamation" aria-hidden="true"></i>
             </argon-button>
             <div class="d-flex flex-column">
-              <h6 class="mb-1 text-dark text-sm">Webflow</h6>
-              <span class="text-xs">26 March 2020, at 05:00 AM</span>
+              <h6 class="mb-1 text-dark text-sm">SPSS</h6>
+              <span class="text-xs">26 Octubre 2022, Hora: 05:00 AM</span>
             </div>
           </div>
-          <div class="d-flex align-items-center text-dark text-sm font-weight-bold">Pending</div>
+          <div class="d-flex align-items-center text-dark text-sm font-weight-bold">Pendiente</div>
         </li>
       </ul>
     </div>
