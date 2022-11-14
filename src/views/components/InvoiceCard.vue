ab424f4d062dfd62e8ce87cda8e6a9ca7df29b11
MavelSterling <mavelynsl@gmail.com>
2022-11-14 01:49:28 -0500

    Se modifica el nombre de la tabla de la sección de ahorros

--- src/views/components/InvoiceCard.vue
+++ src/views/components/InvoiceCard.vue
@@ -3,7 +3,7 @@
     <div class="card-header pb-0 p-3">
       <div class="row">
         <div class="col-6 d-flex align-items-center">
-          <h6 class="mb-0">Invoices</h6>
+          <h6 class="mb-0">Historial de la cuenta</h6>
         </div>
         <div class="col-6 text-end">
           <argon-button color="success" size="sm" variant="outline">View All</argon-button>
