6ac04eb261414e10d94c7bafa725ae3b0fc3693b
MavelSterling <mavelynsl@gmail.com>
2022-11-16 12:39:44 -0500

    actualización de la tabla de historial de usuario

--- src/views/components/InvoiceCard.vue
+++ src/views/components/InvoiceCard.vue
@@ -6,7 +6,7 @@
           <h6 class="mb-0">Historial de la cuenta</h6>
         </div>
         <div class="col-6 text-end">
-          <argon-button color="success" size="sm" variant="outline">View All</argon-button>
+          <argon-button color="success" size="sm" variant="outline">Ver todo</argon-button>
         </div>
       </div>
     </div>
@@ -16,7 +16,7 @@
           class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg"
         >
           <div class="d-flex flex-column">
-            <h6 class="mb-1 text-dark font-weight-bold text-sm">March, 01, 2020</h6>
+            <h6 class="mb-1 text-dark font-weight-bold text-sm">Noviembre, 01, 2022</h6>
             <span class="text-xs">#MS-415646</span>
           </div>
           <div class="d-flex align-items-center text-sm">
@@ -30,7 +30,7 @@
           class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg"
         >
           <div class="d-flex flex-column">
-            <h6 class="text-dark mb-1 font-weight-bold text-sm">February, 10, 2021</h6>
+            <h6 class="text-dark mb-1 font-weight-bold text-sm">Octubre, 25, 2022</h6>
             <span class="text-xs">#RV-126749</span>
           </div>
           <div class="d-flex align-items-center text-sm">
@@ -44,7 +44,7 @@
           class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg"
         >
           <div class="d-flex flex-column">
-            <h6 class="text-dark mb-1 font-weight-bold text-sm">April, 05, 2020</h6>
+            <h6 class="text-dark mb-1 font-weight-bold text-sm">Octubre, 10, 2020</h6>
             <span class="text-xs">#FB-212562</span>
           </div>
           <div class="d-flex align-items-center text-sm">
@@ -58,7 +58,7 @@
           class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg"
         >
           <div class="d-flex flex-column">
-            <h6 class="text-dark mb-1 font-weight-bold text-sm">June, 25, 2019</h6>
+            <h6 class="text-dark mb-1 font-weight-bold text-sm">Octubre, 03, 2022</h6>
             <span class="text-xs">#QW-103578</span>
           </div>
           <div class="d-flex align-items-center text-sm">
@@ -70,7 +70,7 @@
         </li>
         <li class="list-group-item border-0 d-flex justify-content-between ps-0 border-radius-lg">
           <div class="d-flex flex-column">
-            <h6 class="text-dark mb-1 font-weight-bold text-sm">March, 01, 2019</h6>
+            <h6 class="text-dark mb-1 font-weight-bold text-sm">Octubre, 01, 2022</h6>
             <span class="text-xs">#AR-803481</span>
           </div>
           <div class="d-flex align-items-center text-sm">
