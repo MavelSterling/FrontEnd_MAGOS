fc984ba98fe92ee6c2d8982cf69e9a4a0579f2bb
MavelSterling <mavelynsl@gmail.com>
2022-12-13 03:44:15 -0500

    Se crea la pagina de prestamos

---
+++ src/views/Lend.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-8">
+        <div class="row mt-4">
+          <div class="col-xl-6 mb-xl-0 mb-4">
+            <master-card />
+          </div>
+          <div class="col-xl-6">
+            <div class="row">
+              <div class="col-md-6">
+                <default-info-card
+                  :classIcon="salary.classIcon"
+                  :title="salary.title"
+                  :desc="salary.desc"
+                  :price="salary.price"
+                />
+              </div>
+              <div class="col-md-6">
+                <default-info-card
+                  :classIcon="paypal.classIcon"
+                  :title="paypal.title"
+                  :desc="paypal.desc"
+                  :price="paypal.price"
+                />
+              </div>
+            </div>
+          </div>
+         <div class="col-md-13 mb-3">
+            <saving-card/>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <invoice-card class="mt-4" />
+      </div>
+    </div>
+    <div class="row">
+     <!-- <div class="col-md-7">
+        <billing-card />
+      </div>-->
+      <div class="col-md-5">
+        <transaction-card />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+//import MasterCard from "@/examples/Cards/MasterCard.vue";
+import DefaultInfoCard from "@/examples/Cards/DefaultInfoCard.vue";
+import SavingCard from "./components/SavingCard.vue";
+//import InvoiceCard from "./components/InvoiceCard.vue";
+//import BillingCard from "./components/BillingCard.vue";
+import TransactionCard from "./components/TransactionCard.vue";
+
+export default {
+  name: "Lend",
+  components: {
+    /* MasterCard,*/
+    DefaultInfoCard,
+    SavingCard,
+   /* InvoiceCard,*/
+    /*BillingCard,*/
+    TransactionCard,
+  },
+  data() {
+    return {
+      salary: {
+        classIcon: "text-white fas fa-landmark",
+        title: "",
+        desc: "Valor de la deuda:",
+        price: "+$2000",
+      },
+      paypal: {
+        classIcon: "text-white fab fa-paypal",
+        title: "",
+        desc: "Monto abonado:",
+        price: "$455.00",
+      },
+    };
+  },
+};
+</script>
