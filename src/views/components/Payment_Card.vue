babca11113b579919772b10812b2783cfcbe5bcd
MavelSterling <mavelynsl@gmail.com>
2022-12-17 01:53:16 -0500

    Se actualiza la card de abono

--- src/views/components/Payment_Card.vue
+++ src/views/components/Payment_Card.vue
@@ -5,23 +5,6 @@
         <div class="col-3 d-flex align-items-center">
           <h6 class="mb-0">Nuevo Abono</h6>
         </div>
-        <div class="col-3 text-center">
-          <argon-button class="bg-gradient-info text-white">
-           <!--<i class="fas fa-plus me-1"></i>--> 
-            Ingresar abono
-          </argon-button>
-        </div>
-       <!-- <div class="col-3 text-end">
-          <argon-button class="bg-gradient-warning text-white">
-            Actualizar abono
-          </argon-button>
-        </div>
-       <div class="col-3 text-end">
-          <argon-button class="bg-gradient-danger text-white" >
-            Desactivar abono
-
-          </argon-button>
-        </div>-->
       </div>
     </div>
     <div class="card-body p-5">
@@ -99,6 +82,23 @@
         </div>
       </div>
     </div>
+    <div class="col-10 text-end mb-3">
+          <argon-button class="bg-gradient-info text-white">
+           <!--<i class="fas fa-plus me-1"></i>--> 
+            Ingresar abono
+          </argon-button>
+        </div>
+       <!-- <div class="col-3 text-end">
+          <argon-button class="bg-gradient-warning text-white">
+            Actualizar abono
+          </argon-button>
+        </div>
+       <div class="col-3 text-end">
+          <argon-button class="bg-gradient-danger text-white" >
+            Desactivar abono
+
+          </argon-button>
+        </div>-->
   </div>
 </template>
 
