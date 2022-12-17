c33c168e761e6cf72b5914a66549c1ab165fa814
MavelSterling <mavelynsl@gmail.com>
2022-12-17 00:00:33 -0500

    Se actualiza la pagina de prestamos

--- src/views/Lend.vue
+++ src/views/Lend.vue
@@ -1,83 +1,89 @@
 <template>
   <div class="container-fluid">
     <div class="row">
-      <div class="col-lg-8">
+      <div class="col-lg-9">
         <div class="row mt-4">
-          <div class="col-xl-6 mb-xl-0 mb-4">
+          <!--<div class="col-xl-6 mb-xl-0 mb-4">
             <master-card />
-          </div>
+          </div>-->
           <div class="col-xl-6">
             <div class="row">
-              <div class="col-md-6">
+              <!--<div class="col-md-6">
                 <default-info-card
                   :classIcon="salary.classIcon"
                   :title="salary.title"
                   :desc="salary.desc"
                   :price="salary.price"
                 />
-              </div>
-              <div class="col-md-6">
+              </div>-->
+              <!--<div class="col-md-6">
                 <default-info-card
                   :classIcon="paypal.classIcon"
                   :title="paypal.title"
                   :desc="paypal.desc"
                   :price="paypal.price"
                 />
-              </div>
+              </div>-->
             </div>
           </div>
          <div class="col-md-13 mb-3">
-            <saving-card/>
+            <Lend_Card/>
           </div>
         </div>
       </div>
-      <div class="col-lg-4">
+      <!--<div class="col-lg-4">
         <invoice-card class="mt-4" />
-      </div>
+      </div>-->
     </div>
     <div class="row">
+      <div class="col-md-9">
+        <Lend_InfoCard />
+      </div>
      <!-- <div class="col-md-7">
         <billing-card />
-      </div>-->
+      </div>
       <div class="col-md-5">
         <transaction-card />
-      </div>
+      </div>-->
     </div>
   </div>
 </template>
 
 <script>
 //import MasterCard from "@/examples/Cards/MasterCard.vue";
-import DefaultInfoCard from "@/examples/Cards/DefaultInfoCard.vue";
-import SavingCard from "./components/SavingCard.vue";
+//import DefaultInfoCard from "@/examples/Cards/DefaultInfoCard.vue";
+import Lend_Card from "./components/Lend_Card.vue";
+import Lend_InfoCard from "./components/Lend_Info_Card.vue";
 //import InvoiceCard from "./components/InvoiceCard.vue";
 //import BillingCard from "./components/BillingCard.vue";
-import TransactionCard from "./components/TransactionCard.vue";
+//import TransactionCard from "./components/TransactionCard.vue";
 
 export default {
   name: "Lend",
   components: {
     /* MasterCard,*/
-    DefaultInfoCard,
-    SavingCard,
+   /* DefaultInfoCard,*/
+   /* SavingCard,*/
+    Lend_Card,
+    Lend_InfoCard,
    /* InvoiceCard,*/
     /*BillingCard,*/
-    TransactionCard,
+   /* TransactionCard,*/
   },
   data() {
     return {
-      salary: {
+      /*salary: {
         classIcon: "text-white fas fa-landmark",
-        title: "",
-        desc: "Valor de la deuda:",
+        title: "Ahorro:",
+        desc: "Monto del ahorrado:",
         price: "+$2000",
       },
       paypal: {
         classIcon: "text-white fab fa-paypal",
-        title: "",
-        desc: "Monto abonado:",
+        title: "Abonos",
+        desc: "Monto recibido:",
         price: "$455.00",
-      },
+      },*/
     };
   },
 };
