c7317275cebe2bf025b705be258faa1445ba78f2
Juan Diego G <juan.gil.trujillo@correounivalle.edu.co>
2023-01-09 10:54:45 -0500

    implementación interfaz registro abono

--- src/views/components/Payment_Card.vue
+++ src/views/components/Payment_Card.vue
@@ -1,105 +1,51 @@
 <template>
-  <div class="card mt-5">
+  <div class="card mt-5"> 
     <div class="card-header pb-0 p-4">
       <div class="row">
         <div class="col-3 d-flex align-items-center">
-          <h6 class="mb-0">Nuevo Abono</h6>
+          <h6 class="mb-0">Nuevo abono</h6>
         </div>
       </div>
     </div>
-    <div class="card-body p-5">
+
+    <div class="card-body p-5 ">
       <div class="row">
-        <div class="col-md-6 mb-md-0 mb-4">
-          <div
-            class="card card-body border card-plain border-radius-lg d-flex align-items-center flex-row"
-          >
-           <!-- <img class="w-10 me-3 mb-0" src="@/assets/img/logos/mastercard.png" alt="logo" />-->
-            <h6 class="mb-0">Tipo de consignación</h6>
-           <!-- <i
-              class="fas fa-pencil-alt ms-auto text-dark cursor-pointer"
-              data-bs-toggle="tooltip"
-              data-bs-placement="top"
-              title
-              aria-hidden="true"
-              data-bs-original-title="Edit Card"
-              aria-label="Edit Card"
-            ></i>-->
-            <span class="sr-only">Editar tarjeta</span>
-          </div>
-        </div>
-        <div class="col-md-6 ">
-          <div
-            class="card card-body border card-plain border-radius-lg d-flex align-items-center flex-row"
-          >
-            <img class="w-10 me-3 mb-0" src="@/assets/img/logos/dinero.png" alt="logo" />
-            <h6 class="mb-0">$&nbsp;&nbsp;&nbsp;5248</h6>
-            <!--<i
-              class="fas fa-pencil-alt ms-auto text-dark cursor-pointer"
-              data-bs-toggle="tooltip"
-              data-bs-placement="top"
-              title
-              aria-hidden="true"
-              data-bs-original-title="Edit Card"
-              aria-label="Edit Card"
-            ></i>-->
-            <!-- <span class="sr-only">Editar valor</span>-->
-          </div>
-        </div>
-        <div class="col-md-13 mt-3">
-          <div
-            class="card card-body border card-plain border-radius-lg d-flex align-items-center flex-row"
-          >
-           <!-- <img class="w-10 me-3 mb-0" src="@/assets/img/logos/mastercard.png" alt="logo" />-->
-            <h6 class="mb-3">Descripción</h6>
-           <!-- <i
-              class="fas fa-pencil-alt ms-auto text-dark cursor-pointer"
-              data-bs-toggle="tooltip"
-              data-bs-placement="top"
-              title
-              aria-hidden="true"
-              data-bs-original-title="Edit Card"
-              aria-label="Edit Card"
-            ></i>
-            <span class="sr-only">Editar tarjeta</span>-->
-          </div>
-        </div>
-        <div class="col-md-6 mt-3">
-          <div
-            class="card card-body border card-plain border-radius-lg d-flex align-items-center flex-row"
-          >
-            <h6 class="mb-0"> Fecha</h6>
-            
-          
-          </div>
-        </div>
-        <div class="col-md-6 mt-3">
-          <div
-            class="card card-body border card-plain border-radius-lg d-flex align-items-center flex-row"
-          >
-            <h6 class="mb-0">Nombre del abonador</h6>
-      
-          </div>
-        </div>
-      </div>
+
+            <form action="#" class="col-md-6 mb-md-0 mb-4 card card-body border card-plain border-radius-lg d-flex align-items-center flex-row">
+              <label for="lang">Tipo de cuenta a abonar</label>
+              <select name="tipoConsignacion" id="lang" class="col-md-6 mb-md-0 mb-4 card card-body border card-plain border-radius-lg d-flex align-items-center flex-row" v-model="destino">
+                <option value="ahorros" selected="true">Ahorros</option>
+                <option value="prestamos">Préstamos</option>
+                <option value="sanciones">Sanciones</option>
+              </select> 
+            </form>
+
+            <form action="#" class="col-md-6 mb-md-0 mb-4 card card-body border card-plain border-radius-lg d-flex align-items-center flex-row">
+              <label for="lang">Cuenta de ahorros con la que pagará</label>
+              <select name="tipoConsignacion" id="lang" class="col-md-6 mb-md-0 mb-4 card card-body border card-plain border-radius-lg d-flex align-items-center flex-row" v-model="cuenta">
+                <!-- AÑADIR FUNCIONALIDAD BACKEND-->
+                <option value="123" >1412412</option>
+                <option value="123" >1412412</option>
+                <option value="123" >1412412</option>
+              </select> 
+            </form>
+
+            <input class="col-md-6 mb-md-0 mb-4 card card-body border card-plain border-radius-lg d-flex align-items-center flex-row" type="number" placeholder="Monto a abonar" v-model="monto">
+
+            <input class="col-md-6 mb-md-0 mb-4 card card-body border card-plain border-radius-lg d-flex align-items-center flex-row" type="date" placeholder="Monto" v-model="fecha">
+
+            <input class="col-md-6 mb-md-0 mb-4 card card-body border card-plain border-radius-lg d-flex align-items-center flex-row" type="text" placeholder="Descripción" v-model="descripcion">
+ 
+
     </div>
     <div class="col-10 text-end mb-3">
           <argon-button class="bg-gradient-info text-white">
            <!--<i class="fas fa-plus me-1"></i>--> 
             Ingresar abono
           </argon-button>
-        </div>
-       <!-- <div class="col-3 text-end">
-          <argon-button class="bg-gradient-warning text-white">
-            Actualizar abono
-          </argon-button>
-        </div>
-       <div class="col-3 text-end">
-          <argon-button class="bg-gradient-danger text-white" >
-            Desactivar abono
-
-          </argon-button>
-        </div>-->
+        </div> 
   </div>
+  </div> 
 </template>
 
 <script>
