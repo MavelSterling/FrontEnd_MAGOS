c2b0059b95b287f8915735dfdb67b7d3a3ba7105
Juan Diego G <juan.gil.trujillo@correounivalle.edu.co>
2023-01-09 22:45:55 -0500

    implementación estable de las dependencias necesarias para que funcione Jest con Vue y todas sus dependencias (incluyen mocks para simulares los atributos propios del componente montado). Y sólo me tomó 5 horas

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
   transformIgnorePatterns: ['/node_modules/(?!(axios)/)'],
   transform: {
-    '^.+\\.vue$': 'vue3-jest'
-  } 
+    '^.+\\.vue$': 'vue3-jest',
+    '^.+\\js$': 'babel-jest',
+  },
 }
--- tests/unit/views/signin.spec.js
+++ tests/unit/views/signin.spec.js
@@ -1,11 +1,35 @@
-
+import { shallowMount, mount } from '@vue/test-utils';
+import Signin from "@/views/Signin"
+import router from "@/router";
 
 describe('Signin Component', () => {
-  
-  test('prueba #1 simple', () => {
-    const uno = 1
 
-    console.log("uno", uno)
+  const mocks = { 
+    $store : {
+      state : {
+        hideConfigButton: '',
+        showNavbar: '',
+        showSidenav: '',
+        showFooter: ''
+      }
+    } 
+  }
+
+  const mountOptions = {
+    global: {
+      mocks ,
+      plugins : [router]
+    }
+  }
+ 
+
+  
+
+  const wrapper = mount( Signin, mountOptions )
+  
+  test('prueba #1 simple', () => { 
+    console.log('hola')
+    const uno = 1 
     expect( uno ).toBe(1)
   })
 })
